126e7b0fde3aef1305d8443cfd975ae0ce717c2a
Tomasz Bortniak <tommy@Tomaszs-MacBook-Air.local>
2023-02-26 16:40:57 +0100

    Add initial project code

---
+++ Recipes/RecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipesApp.swift
+//  Recipes
+//
+//  Created by Tomasz Bortniak on 26/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct RecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
