5481e19073b78eb92048b2d39ce84846d06e0147
tombortniak <tombortniak@gmail.com>
2023-02-27 20:43:58 +0100

    Add recipe model

---
+++ Recipes/Models/Recipe.swift
@@ -0,0 +1,13 @@
+//
+//  Recipe.swift
+//  Recipes
+//
+//  Created by Tomasz Bortniak on 27/02/2023.
+//
+
+import Foundation
+
+struct Recipe {
+    let title: String
+    let duration: Duration
+}
