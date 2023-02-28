d8cbf8a383cf78c913c1bac2b8bc798c41060552
tombortniak <tombortniak@gmail.com>
2023-02-28 20:25:57 +0100

    Add ingredient model

---
+++ Recipes/Models/Ingredient.swift
@@ -0,0 +1,14 @@
+//
+//  Ingredient.swift
+//  Recipes
+//
+//  Created by Tomasz Bortniak on 28/02/2023.
+//
+
+import Foundation
+
+struct Ingredient {
+    var name: String
+    var amount: Double
+    var unit: String
+}
